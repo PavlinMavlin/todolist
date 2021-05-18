0006f1e859bcc00f56889611833ac42b6f078e0a
PavlinMavlin <korobovpavel2017@yandex.by>
2021-05-18 12:49:01 +0300

    Updates

---
+++ precache-manifest.b366a3fb40a6f5ee4bf2955513c608ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd184ba94fea2730c953e5733c1e622f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "22854b277c54e119efa1",
+    "url": "/todolist/static/css/main.a392944d.chunk.css"
+  },
+  {
+    "revision": "69477342030bce20d170",
+    "url": "/todolist/static/js/2.6efd56d4.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.6efd56d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22854b277c54e119efa1",
+    "url": "/todolist/static/js/main.b80d7284.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
